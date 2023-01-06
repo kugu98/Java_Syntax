3ec3a07ced3b6e3c2d600aeb4a5480ef63b0bf2f
gu <82009882+kugu98@users.noreply.github.com>
2023-01-06 12:13:34 +0900

    Scanner
    Scanner/nextLine()/println/printf

---
+++ src/Scanner.java
@@ -0,0 +1,12 @@
+public class Scanner {
+    public static void main(String[] args) {
+        java.util.Scanner scanner=new java.util.Scanner(System.in);
+
+        System.out.print("두자리 정수를 하나 입력해주세요.>");
+        String input=scanner.nextLine();
+        int num=Integer.parseInt(input);
+
+        System.out.println("입력내용:"+input);
+        System.out.printf("num=%d",num);
+    }
+}
