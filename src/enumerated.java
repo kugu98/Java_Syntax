a363fb49a10bc61281807f6518e16f640c6cd0a5
gu <82009882+kugu98@users.noreply.github.com>
2023-01-14 15:44:37 +0900

    Enum
    class 와 차이

---
+++ src/enumerated.java
@@ -0,0 +1,12 @@
+enum enumerated {
+    //서로 연관된 상수들의 집합
+    //class 와 Enum 의 차이점
+
+}
+class Fruitclass{
+    public static final Fruitclass APPLE=new Fruitclass();
+}
+enum Fruit{
+    APPLE,PEACH,BANANA;
+}
+
