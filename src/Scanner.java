fc2d2c200450e03f036b6e505a872fa384e518ec
gu <82009882+kugu98@users.noreply.github.com>
2023-06-26 16:09:29 +0900

    BS
    BufferedReader, StringTokenizer

--- src/Scanner.java
+++ src/Scanner.java
@@ -1,3 +1,5 @@
+import java.util.Date;
+
 public class Scanner {
     public static void main(String[] args) {
         java.util.Scanner scanner=new java.util.Scanner(System.in);
@@ -5,7 +7,7 @@ public class Scanner {
         System.out.print("두자리 정수를 하나 입력해주세요.>");
         String input=scanner.nextLine();
         int num=Integer.parseInt(input);
-
+        
         System.out.println("입력내용:"+input);
         System.out.printf("num=%d",num);
     }
